6d8099834c7c1fe06ffa35ca91bd28d90058bc89
kentotakeuchi <ideal.kt.reality@gmail.com>
2018-09-21 17:56:45 -0700

    Add form validation on the login/join modal.

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,71 +1,281 @@
-/**
- * Transition between join and login
- */
+// Variable to store HTML element references, for greater code clarity.
+var ELEM = {};
 
-$(function() {
-    // Showing Join Modal
-    $("#join_modal").click(toggleLoginJoin);
+// Perform tasks that are dependent on the HTML being rendered (being 'ready').
+$('document').ready(function() {
+    // Capture HTML element references.
+    getElementReferences();
 
-    // Showing Login Modal
-    $("#login_modal").click(toggleLoginJoin);
+    // Set event handlers.
+    setEventHandlers();
+});
 
-    function toggleLoginJoin () {
-        $('.loginModal').modal('toggle');
-        $('.joinModal').modal('toggle');
+// Capture HTML element references.
+function getElementReferences() {
+    // Register and login links.
+    ELEM.registerCtrl = $('#registerCtrl');
+    ELEM.loginCtrl = $('#loginCtrl');
+    ELEM.joinLoginCtrl = $('#joinLoginCtrl');
+
+    // User register modal.
+    ELEM.registerUserModal = $('#registerUserModal');
+    ELEM.registerModalErrorMessageContainer = $('#registerModalErrorMessageContainer');
+    ELEM.registerModalErrorMessage = $('#registerModalErrorMessage');
+    ELEM.registerEmailInput = $('#registerEmailInput');
+    ELEM.registerPasswordInput = $('#registerPasswordInput');
+    ELEM.registerPasswordConfirmInput = $('#registerPasswordConfirmInput');
+    ELEM.loginLink = $('#loginLink');
+    ELEM.registerBtn = $('#registerBtn');
+
+    // User login modal.
+    ELEM.loginUserModal = $('#loginUserModal');
+    ELEM.loginModalErrorMessageContainer = $('#loginModalErrorMessageContainer');
+    ELEM.loginModalErrorMessage = $('#loginModalErrorMessage');
+    ELEM.loginEmailInput = $('#loginEmailInput');
+    ELEM.loginPasswordInput = $('#loginPasswordInput');
+    ELEM.registerLink = $('#registerLink');
+    ELEM.loginBtn = $('#loginBtn');
+}
+
+// Set event handlers.
+function setEventHandlers() {
+    ELEM.registerLink.click(registerClickHandler);
+    ELEM.loginLink.click(loginClickHandler);
+    ELEM.joinLoginCtrl.click(joinLoginClickHandler);
+
+    ELEM.registerEmailInput.change(checkRegisterData);
+    ELEM.registerEmailInput.keyup(checkRegisterData);
+    ELEM.registerPasswordInput.change(checkRegisterData);
+    ELEM.registerPasswordInput.keyup(checkRegisterData);
+    ELEM.registerPasswordConfirmInput.change(checkRegisterData);
+    ELEM.registerPasswordConfirmInput.keyup(checkRegisterData);
+    ELEM.registerBtn.click(registerUserHandler);
+    // Ensure when the modal appears cursor is in email field.
+    ELEM.registerUserModal.on('shown.bs.modal', function () {
+        ELEM.registerEmailInput.trigger('focus')
+    });
+
+    ELEM.loginEmailInput.change(checkLoginData);
+    ELEM.loginEmailInput.keyup(checkLoginData);
+    ELEM.loginPasswordInput.change(checkLoginData);
+    ELEM.loginPasswordInput.keyup(checkLoginData);
+    ELEM.loginBtn.click(loginUserHandler);
+    // Ensure when the modal appears cursor is in email field.
+    ELEM.loginUserModal.on('shown.bs.modal', function () {
+        ELEM.loginEmailInput.trigger('focus')
+    });
+}
+
+// Handler for link clicked to show the user register modal.
+function registerClickHandler() {
+    // Fields should be blank when modal opens.
+    resetValues();
+
+    // Reset modal messages.
+    resetModalMessages();
+
+    // Close the login modal and show the register modal.
+    ELEM.loginUserModal.modal('toggle');
+    ELEM.registerUserModal.modal('toggle');
+}
+
+// Handler for link clicked to show the user login modal.
+function loginClickHandler() {
+    // Fields should be blank when modal opens.
+    resetValues();
+
+    // Reset modal messages.
+    resetModalMessages();
+
+    // Close the register modal and show the login modal.
+    ELEM.registerUserModal.modal('toggle');
+    ELEM.loginUserModal.modal('toggle');
+}
+
+// Handler for button clicked to show the user login modal.
+function joinLoginClickHandler() {
+    // Fields should be blank when modal opens.
+    resetValues();
+
+    // Reset modal messages.
+    resetModalMessages();
+
+    // Show the modal.
+    ELEM.loginUserModal.modal('toggle');
+}
+
+// Handler to register user.
+function registerUserHandler () {
+    // Temporarily capture data from modal.
+    var email = ELEM.registerEmailInput.val();
+    var password = ELEM.registerPasswordInput.val();
+
+    // Call server API to register the user.
+    registerUser(email, password);
+}
+
+// Handler to login user.
+function loginUserHandler () {
+    // Temporarily capture data from modal.
+    var email = ELEM.loginEmailInput.val();
+    var password = ELEM.loginPasswordInput.val();
+
+    // Validate email address format and password.
+    if (!loginEmailValid(email)) {
+        ELEM.loginModalErrorMessageContainer.addClass('dangerColor');
+        ELEM.loginModalErrorMessage.html('Email format is incorrect.');
+        ELEM.loginModalErrorMessageContainer.css('display', 'inline-block');
+        return;
     }
-});
 
+    // Verify password field is not empty.
+    if (!validatePasswordsNotEmpty(password)) {
+        ELEM.registerModalErrorMessageContainer.addClass('dangerColor');
+        ELEM.registerModalErrorMessage.html('Password is required.');
+        ELEM.registerModalErrorMessageContainer.css('display', 'inline-block');
+        return;
+    }
 
-/**
- * Input(login, join) --> console.log with js
- */
+    // Reset modal messages.
+    resetModalMessages();        
 
-// Global
-var lgEmailInput = document.getElementById('login-email');
-var joinEmailInput = document.getElementById('join-email');
-var pwdInput = document.getElementById('pwd');
+    // Call server to see if email is already registered.
 
-/*
-// Click login button
-document.querySelector('.lg-btn').addEventListener('click', function(event) {
-    event.preventDefault();
-    var email = lgEmailInput.value;
-    var password = pwdInput.value;
-    console.log("email", email);
-    console.log("password", password);
-});
+    // Call server API to login the user.
+    loginUser(email, password);
+}
 
-// Click join button
-document.querySelector('.join-btn').addEventListener('click', function(event) {
-    event.preventDefault();
-    var email = joinEmailInput.value;
-    var password = pwdInput.value;
-    console.log("email", email);
-    console.log("password", password);
-});
-*/
+function registerUser() {
+    console.log('registering user');
+}
 
-/**
- * Input --> console.log with jquery
- */
+function loginUser() {
+    console.log('logging in user');
+}
 
-$(function() {
-    // Showing Join Modal
-    $("#lg-btn").click(function() {
-        var email = lgEmailInput.value;
-        var password = pwdInput.value;
-        console.log("email", email);
-        console.log("password", password);
-    });
+function checkRegisterData() {
+    if (!registerEmailValid(ELEM.registerEmailInput.val()) ||
+        !registerPasswordFormatCorrect(ELEM.registerPasswordInput.val())  || 
+        ELEM.registerEmailInput.val() === '' ||
+        ELEM.registerPasswordInput.val() === '' ||
+        ELEM.registerPasswordConfirmInput.val() === '' ||        
+        !registerPasswordsMatch(
+            ELEM.registerPasswordInput.val(),
+            ELEM.registerPasswordConfirmInput.val()
+        )   
+    ) {
+        ELEM.registerBtn.prop('disabled', true);
+    } else {
+        ELEM.registerBtn.prop('disabled', false);
+    }
+}
 
-    // Showing Login Modal
-    $("#join-btn").click(function() {
-        var email = joinEmailInput.value;
-        var password = pwdInput.value;
-        console.log("email", email);
-        console.log("password", password);
-    });
+function checkLoginData() {
+    if (!loginEmailValid(ELEM.loginEmailInput.val()) ||
+        ELEM.loginEmailInput.val() === '' ||
+        ELEM.loginPasswordInput.val() === ''
+    ) {
+        ELEM.loginBtn.prop('disabled', true);
+    } else {
+        ELEM.loginBtn.prop('disabled', false);
+    }
+}
 
-});
+function resetValues() {
+    ELEM.registerEmailInput.val('');
+    ELEM.registerPasswordInput.val('');
+    ELEM.registerPasswordConfirmInput.val('');
 
+    ELEM.loginEmailInput.val('');
+    ELEM.loginPasswordInput.val('');
 
+    checkRegisterData();
+    checkLoginData();
+}
+
+function resetModalMessages() {
+    ELEM.registerModalErrorMessageContainer.css('display', 'none');
+    ELEM.registerModalErrorMessageContainer.removeClass('dangerColor warningColor infoColor okColor');
+    ELEM.registerModalErrorMessage.html('');
+    ELEM.loginModalErrorMessageContainer.css('display', 'none');
+    ELEM.loginModalErrorMessageContainer.removeClass('dangerColor warningColor infoColor okColor');
+    ELEM.loginModalErrorMessage.html('');
+}
+
+function registerEmailValid(emailAddress) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var valid = true;
+
+    if (emailAddress.length > 0) {
+        valid = re.test(String(emailAddress).toLowerCase());
+    }
+
+    if (!valid) {
+        ELEM.registerModalErrorMessageContainer.addClass('dangerColor');
+        ELEM.registerModalErrorMessage.html('Email format is incorrect.');
+        ELEM.registerModalErrorMessageContainer.css('display', 'inline-block');
+    } else {
+        resetModalMessages();
+    }
+
+    return valid;
+}
+
+function registerPasswordsMatch(password1, password2) {
+    var valid = password1 === password2;
+
+    if (!valid) {
+        ELEM.registerModalErrorMessageContainer.addClass('dangerColor');
+        ELEM.registerModalErrorMessage.html('Password and confirm password fields do not match.');
+        ELEM.registerModalErrorMessageContainer.css('display', 'inline-block');
+    } else {
+        resetModalMessages();
+    }
+
+    return valid;
+}
+
+function registerPasswordFormatCorrect(password) {
+    var lowerCaseRegex = /.*[a-z].*/;
+    var upperCaseRegex = /.*[A-Z].*/;
+    var numberRegex = /.*\d.*/;
+    var symbolRegex = /.*[!@#$%^&*+?].*/;
+    var valid = true;
+    
+    if (password.length > 0) {
+        valid = password.length >= 8 &&
+        lowerCaseRegex.test(String(password)) &&
+        upperCaseRegex.test(String(password)) &&
+        numberRegex.test(String(password)) &&
+        symbolRegex.test(String(password));
+    }
+
+    if (!valid) {
+        ELEM.registerModalErrorMessageContainer.addClass('dangerColor');
+        ELEM.registerModalErrorMessage.html('Password must be at least 8 characters in length, and must have at least one number, one uppercase letter, one lowercase letter, and one of the following symbols: ! @ # $ % ^ & * + ?');
+        ELEM.registerModalErrorMessageContainer.css('display', 'inline-block');
+    } else {
+        resetModalMessages();
+    }
+
+    return valid;
+}
+
+function loginEmailValid(emailAddress) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var valid = true;
+
+    if (emailAddress.length > 0) {
+        valid = re.test(String(emailAddress).toLowerCase());
+    }
+
+    if (!valid) {
+        ELEM.loginModalErrorMessageContainer.addClass('dangerColor');
+        ELEM.loginModalErrorMessage.html('Email format is incorrect.');
+        ELEM.loginModalErrorMessageContainer.css('display', 'inline-block');
+    } else {
+        resetModalMessages();
+    }
+
+    return valid;
+}
--- client/assets/js/mypage.js
+++ client/assets/js/mypage.js
@@ -54,6 +54,10 @@ function setEventHandlers() {
     ELEM.itemCnd.change(checkData);
     ELEM.addItemBtn.click(addItemHandler);
     ELEM.saveItemBtn.click(saveItemHandler);
+    // Ensure when the modal appears cursor is in name field.
+    ELEM.addEditModal.on('shown.bs.modal', function() {
+        ELEM.itemName.trigger('focus');
+    });
 }
 
 // Handler for button clicked to show the add item modal.
@@ -66,9 +70,6 @@ function addItemHandler() {
 
     // Show the modal.
     ELEM.addEditModal.modal('toggle');
-
-    // Ensure when the modal appears cursor is in name field.
-    ELEM.itemName.focus();// not work
 }
 
 // Handler to save data from the add/edit modal.
@@ -262,9 +263,6 @@ function editItem(event) {
     
     // Show the modal.
     ELEM.addEditModal.modal('toggle');
-
-    // Ensure when the modal appears cursor is in name field.
-    ELEM.itemName.focus();
 }
 
 // Reset the modal fields, and the item to edit id.
@@ -281,232 +279,5 @@ function resetValues() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// after loading the page
-$('document').ready(function() {
-    checkData();
-    $('#saveItemBtn').prop('disable', true);
-    generateItems();
-});
-
-$('#item-img').change(function() {
-    checkData();
-});
-$('#item-name').change(function() {
-    checkData();
-});
-$('#item-desc').change(function() {
-    checkData();
-});
-$('#item-brand').change(function() {
-    checkData();
-});
-$('#item-ctg').change(function() {
-    checkData();
-});
-$('#item-cnd').change(function() {
-    checkData();
-});
-
-// Open add item modal
-$('#addItemBtn').click(addItemHandler);
-function addItemHandler() {
-    $('.modal-title').html('ADD ITEM');
-    $('#item-img').val('');
-    $('#item-name').val('');
-    $('#item-desc').val('');
-    $('#item-brand').val('');
-    $('#item-ctg').val('');
-    $('#item-cnd').val('');
-    $('#addEditModal').modal('toggle');
-}
-
-// Whether ADD button is disable or not
-function checkData() {
-    var itemImg = $('#item-img');
-    var itemName = $('#item-name');
-    var itemDesc = $('#item-desc');
-    var itemBrand = $('#item-brand');
-    var itemCtg = $('#item-ctg');
-    var itemCnd= $('#item-cnd');
-    if (itemImg.val() === '' || itemName.val() === '' || itemDesc.val() === '' || itemBrand.val() === '' || itemCtg.val() === '' || itemCnd.val() === '') {
-        $('#saveItemBtn').prop('disabled', true);
-    } else {
-        $('#saveItemBtn').prop('disabled', false);
-    }
-}
-
-
-var itemsData = [{
-    id: 1,
-    name: 'Sample Item One',
-    description: 'Description of sample item one.'
-}, {
-    id: 2,
-    name: 'Sample Item Two',
-    description: 'Description of sample item two.'
-}];
-
-// Save item
-$('#saveItemBtn').click(function() {
-    var img = $('#item-img').val();
-    var name = $('#item-name').val();
-    var desc = $('#item-desc').val();
-    var brand = $('#item-brand').val();
-    var ctg = $('#item-ctg').val();
-    var cnd = $('#item-cnd').val();
-    saveItem(img, name, desc, brand, ctg, cnd);
-});
-function saveItem(iImg, iName, iDesc, iBrand, iCtg, iCnd) {
-    var idOfItemToEdit = parseInt($('#idOfItemBeingEdited').val());
-    var dataOfItemToEdit;
-    itemsData.forEach(function(item) {
-        if (item.id === idOfItemToEdit) {
-            dataOfItemToEdit = item;
-        }
-    });
-    if (dataOfItemToEdit) {
-        dataOfItemToEdit.img = $('#item-img').val();
-        dataOfItemToEdit.name = $('#item-name').val();
-        dataOfItemToEdit.description = $('#item-desc').val();
-        dataOfItemToEdit.brand = $('#item-brand').val();
-        dataOfItemToEdit.ctg = $('#item-ctg').val();
-        dataOfItemToEdit.cnd = $('#item-cnd').val();
-    } else {
-        itemsData.push({
-            id: itemsData.length + 1,
-            img: iImg,
-            name: iName,
-            description: iDesc,
-            brand: iBrand,
-            ctg: iCtg,
-            cnd: iCnd
-        });
-    }
-    generateItems();
-    clearForm();
-}
-
-function clearForm() {
-    $('#item-img').val('');
-    $('#item-name').val('');
-    $('#item-desc').val('');
-    $('#item-brand').val('');
-    $('#item-ctg').val('');
-    $('#item-cnd').val('');
-    checkData();
-    $('#idOfItemBeingEdited').val('');
-}
-
-// Render items
-function generateItems() {
-    $('#items').empty();
-    itemsData.forEach(function(item) {
-        var divElement = $('<div class="item" id="' + item.id + '"></div>');
-        
-        var toolsContainer = $('<div class="tools-container"></div>')
-        var removeIcon = '<i class="fa fa-times" aria-hidden="true" id="removeIcon"></i>';
-        var editIcon = '<i class="fas fa-pen" data-toggle="modal" data-target="#editItemModal" id="editIcon"></i>';
-        toolsContainer.append(editIcon);
-        toolsContainer.append(removeIcon);
-        
-        var imgElement = '<img>' + item.img + '</img>';
-        var nameElement = '<h4>' + item.name + '</h4>';
-        var descElement = '<p>' + item.description + '</p>';     
-        var brandElement = '<p>' + item.brand + '</p>';                
-        var ctgElement = '<p>' + item.ctg + '</p>';                
-        var cndElement = '<p>' + item.cnd + '</p>';  
-        divElement.append(toolsContainer);              
-        divElement.append(imgElement);
-        divElement.append(nameElement);
-        divElement.append(descElement);
-        divElement.append(brandElement);
-        divElement.append(ctgElement);
-        divElement.append(cndElement);
-        $('#items').append(divElement);
-        
-        $('#' + item.id).find('#removeIcon').click(removeItem);
-        $('#' + item.id).find('#editIcon').click(editItem);
-    });
-}
-
-// Remove item handler
-function removeItem(event) {
-    var itemToRemove = $(event.target).parent().parent();
-    var idOfItemToRemove = parseInt($(itemToRemove).attr('id'));
-    itemsData = itemsData.filter(function(item) {
-        return item.id !== idOfItemToRemove;
-    });
-    generateItems();
-}
-
-// Open edit item modal
-function editItem(event) {
-    var itemToEdit = $(event.target).parent().parent();
-    var idOfItemToEdit = parseInt($(itemToEdit).attr('id'));
-    $('#addEditModalTitle').html('EDIT ITEM');
-    $('#idOfItemBeingEdited').val(idOfItemToEdit);
-    var dataOfItemToEdit;
-    itemsData.forEach(function(item) {
-        if (item.id === idOfItemToEdit) {
-            dataOfItemToEdit = item;
-        }
-    });
-    if (dataOfItemToEdit) {
-        // $('#item-img').val(dataOfItemToEdit.img);
-        $('#item-name').val(dataOfItemToEdit.name);
-        $('#item-desc').val(dataOfItemToEdit.description);
-        $('#item-brand').val(dataOfItemToEdit.brand);
-        $('#item-ctg').val(dataOfItemToEdit.ctg);
-        $('#item-cnd').val(dataOfItemToEdit.cnd);
-        checkData();
-    }
-    $('#addEditModal').modal('toggle');
-}
-*/
 
 
